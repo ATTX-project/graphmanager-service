c875309ec0c088d6910ad486fff0d21d761700c0
blankdots <stefan@blankdots.com>
2017-09-18 10:58:09 +0300

    adding shared dir as env variable

--- src/graph_manager/utils/file.py
+++ src/graph_manager/utils/file.py
@@ -2,11 +2,13 @@ import uuid
 import os
 from graph_manager.utils.logs import app_logger
 
+data = {'directory': os.environ['DATADIR'] if 'DATADIR' in os.environ else "/attx-sb-shared"}
+
 
 def results_path(content, extension):
     """Write results to specific file."""
     try:
-        path = "/attx-sb-shared/graphmanager/{0}".format(uuid.uuid4().hex)
+        path = "{0}/graphmanager/{1}".format(data["directory"], uuid.uuid4().hex)
         if not os.path.exists(path):
             os.makedirs(path)
         full_path = "{0}/{1}.{2}".format(path, uuid.uuid1().hex, extension)
