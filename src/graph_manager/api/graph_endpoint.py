5e26e7eec49c4dceeee65ba34c4681f49c4e37ee
Joonas Kesäniemi <joonas.kesaniemi@csc.fi>
2018-03-09 16:31:38 +0200

    Update graph_endpoint.py

--- src/graph_manager/api/graph_endpoint.py
+++ src/graph_manager/api/graph_endpoint.py
@@ -39,7 +39,7 @@ class GraphResource(object):
         fuseki = GraphStore()
         response = fuseki._graph_retrieve(graph_uri)
         if response is not None:
-            resp.data = str(response)
+            resp.data = response
             resp.content_type = 'text/turtle'
             app_logger.info('Retrieved: {0}.'.format(graph_uri))
             resp.status = falcon.HTTP_200
